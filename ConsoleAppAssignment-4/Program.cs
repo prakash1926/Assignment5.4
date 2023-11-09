e0065b0806ff16c0e27c081bb72c328ed4f3b3b1
prakash1926 <ananthaprakash71@gmail.com>
2023-11-09 16:44:20 +0530

    Assignment 5.4 First commit

---
+++ ConsoleAppAssignment-4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            Console.WriteLine("Enter Random integers :");
+
+            for(int i=0; i<array.Length; i++) 
+            {
+                int myarray =int.Parse(Console.ReadLine());
+                array[i] = myarray;
+            }
+            Console.WriteLine("Populated array :");
+           
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+
+            }
+            int sum = 0;
+            Console.WriteLine("\nsum of array elements :");
+            foreach (int num in array) 
+            {
+               
+                sum = sum + num;
+            }
+            double average=sum/array.Length;
+
+            Console.WriteLine(sum + "\n ");
+            Console.WriteLine("Average of the elements : "+average);
+            Console.ReadKey();
+        }
+    }
+}
